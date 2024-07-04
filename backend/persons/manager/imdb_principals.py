206df5d44d411049e7a8579ee26e8e394b21c702
Ivan Tomilov <tomilov.vana@gmail.com>
2024-07-04 22:32:26 +0500

    Reworked db api. Reworked init data functions. Reworked db managers.

--- backend/persons/manager/imdb_principals.py
+++ backend/persons/manager/imdb_principals.py
@@ -2,14 +2,18 @@ import sys
 import asyncio
 from functools import wraps
 from pathlib import Path
+from tqdm.asyncio import tqdm_asyncio
 from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 ROOT_DIR = Path(__file__).parent.parent
 PROJ_DIR = ROOT_DIR.parent
 sys.path.append(str(ROOT_DIR))
 sys.path.append(str(PROJ_DIR))
+sys.path.append(str(PROJ_DIR.parent))
 
+from backend.settings import settings
 from database.api import ExceptionToHandle
 from movies.orm import IMDbMovieORM
 from movies.manager.imdb_movie import IMDbMovieManager
@@ -41,50 +45,97 @@ class IMDbMoviePrincipalManager(DataBaseManager):
             exceptions_to_handle=exceptions_to_handle,
         )
 
+        semlimit = (settings.PG_POOL_SIZE + settings.PG_MAX_OVERFLOW) // 2
+        self.semaphore = asyncio.Semaphore(semlimit)
+
+    async def get_movie_id(
+        self,
+        principal: IMDbPrincipalSourceDM,
+        session: AsyncSession,
+    ) -> int | None:
+        imdb_movie = await self.dbapi.get(
+            IMDbMovieORM,
+            session,
+            IMDbMovieORM.id,
+            imdb_mvid=principal.imdb_movie,
+        )
+
+        imdb_movie_id = imdb_movie.id if imdb_movie else None
+        if imdb_movie_id is None:
+            return None
+        return imdb_movie_id
+
+    async def get_person_id(
+        self,
+        principal: IMDbPrincipalSourceDM,
+        session: AsyncSession,
+    ) -> int | None:
+        imdb_person = await self.dbapi.get(
+            IMDbPersonORM,
+            session,
+            IMDbPersonORM.id,
+            imdb_nmid=principal.imdb_person,
+        )
+
+        imdb_person_id = imdb_person.id if imdb_person else None
+        if imdb_person_id is None:
+            return None
+        return imdb_person_id
+
+    async def get_category_id(
+        self,
+        principal: IMDbPrincipalSourceDM,
+        session: AsyncSession,
+    ) -> int | None:
+        category = await self.dbapi.get(
+            ProfessionORM,
+            session,
+            ProfessionORM.id,
+            imdb_name=principal.category.imdb_name,
+        )
+
+        category_id = category.id if category else None
+        if category_id is None:
+            return None
+        return category_id
+
+    async def mark_up(
+        self,
+        imdb_id: int,
+        session: AsyncSession,
+    ) -> None:
+        await self.dbapi.update(
+            IMDbMovieORM,
+            session,
+            filters={"id": imdb_id},
+            principals_added=True,
+        )
+
     async def add(self, principal: IMDbPrincipalSourceDM) -> None:
-        async with self.dbapi.session as session:
-            movie = await self.dbapi.get(
-                IMDbMovieORM,
-                session,
-                IMDbMovieORM.id,
-                imdb_mvid=principal.imdb_movie,
-            )
+        async with self.semaphore:
+            async with self.dbapi.session as session:
+                movie_id = await self.get_movie_id(principal, session)
+                if not movie_id:
+                    return
 
-            movie_id = movie.id if movie else None
-            if movie_id is None:
-                return None
+                person_id = await self.get_person_id(principal, session)
+                if not person_id:
+                    return
 
-            person = await self.dbapi.get(
-                IMDbPersonORM,
-                session,
-                IMDbPersonORM.id,
-                imdb_nmid=principal.imdb_person,
-            )
+                category_id = await self.get_category_id(principal, session)
+                if not category_id:
+                    return
 
-            person_id = person.id if person else None
-            if person_id is None:
-                return None
-
-            category = await self.dbapi.get(
-                ProfessionORM,
-                session,
-                ProfessionORM.id,
-                imdb_name=principal.category.imdb_name,
-            )
-
-            category_id = category.id if category else None
-            if category_id is None:
-                return None
-
-            await self.dbapi.add(
-                MoviePrincipalORM,
-                session,
-                _safe_add=True,
-                imdb_movie=movie_id,
-                imdb_person=person_id,
-                category=category_id,
-                **principal.to_db(),
-            )
+                await self.dbapi.add(
+                    MoviePrincipalORM,
+                    session,
+                    _safe_add=True,
+                    imdb_movie=movie_id,
+                    imdb_person=person_id,
+                    category=category_id,
+                    **principal.to_db(),
+                )
+                await self.mark_up(movie_id, session)
 
 
 async def movie_principals_init():
@@ -104,7 +155,8 @@ async def movie_principals_init():
         imdb_mvids=[m.imdb_mvid for m in imdbs]
     )
 
-    await manager.add(principals[0])
+    tasks = [asyncio.create_task(manager.add(p)) for p in principals]
+    await tqdm_asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
